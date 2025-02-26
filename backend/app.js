73f298eefd470b7cc258529f9b5da879f097f01b
Thuandq <thuandq@smartosc.com>
2025-02-26 18:57:50 +0700

    add data to database

--- backend/app.js
+++ backend/app.js
@@ -8,49 +8,54 @@ import cors from 'cors';
 dotenv.config();
 
 async function main() {
-    console.log(`🎧 Listening for events on contract: ${process.env.CONTRACT_ADDRESS}`);
-    // contract.events.allEvents()
-    // .on('data', (event) => {
-    //   console.log("📢 Event detected:", event);
-    // })
-    // .on('error', (error) => {
-    //   console.error("❌ Error:", error);
-    // });
+  console.log(`🎧 Listening for events on contract: ${process.env.CONTRACT_ADDRESS}`);
+  // contract.events.allEvents()
+  // .on('data', (event) => {
+  //   console.log("📢 Event detected:", event);
+  // })
+  // .on('error', (error) => {
+  //   console.error("❌ Error:", error);
+  // });
 
-
-    contract.events.TokenCreated({fromBlock: "latest"})
-        .on("data", (event) => {
-            console.log(`🔥 New Transfer detected!`);
-            console.log(`🔹 From: ${event.returnValues.from}`);
-            console.log(`🔹 To: ${event.returnValues.to}`);
-            console.log(`🔹 Token ID: ${event.returnValues.tokenId}`);
-            console.log("----------------------------");
-            console.log("save to db");
-
-        })
-        .on("error", (error) => {
-            console.error("❌ Error:", error);
+  contract.events.TokenCreated({ fromBlock: "latest" })
+    .on("data", (event) => {
+      console.log(`🔥 New Token Created!`);
+      console.log("----------------------------");
+      const query = 'INSERT INTO tokens (creator, token_address, name, symbol, total_token) VALUES (?, ?, ?, ?, ?)';
+      connection.query(query,
+        [event.returnValues.creator, event.returnValues.tokenAddress, event.returnValues.name, event.returnValues.symbol, 0],
+        (err, result) => {
+          if (err) {
+            console.error('Error saving token creation data:', err);
+          } else {
+            console.log('Token creation data saved success:', result);
+          }
         });
+
+    })
+    .on("error", (error) => {
+      console.error("❌ Error:", error);
+    });
 }
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        const query = 'SELECT * FROM tokens';
-        connection.query(query, (err, results) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({error: 'Server error '});
-            } else {
-                res.json(results);
-            }
-        });
-
-    } catch (err) {
+  try {
+    const query = 'SELECT * FROM tokens';
+    connection.query(query, (err, results) => {
+      if (err) {
         console.error(err);
-        res.status(500).json({error: 'Server error '});
-    }
+        res.status(500).json({ error: 'Server error ' });
+      } else {
+        res.json(results);
+      }
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error ' });
+  }
 });
 
 const app = express();
@@ -59,9 +64,9 @@ app.use(express.json());
 
 app.use('/api/tokens', router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 main().catch(console.error);
